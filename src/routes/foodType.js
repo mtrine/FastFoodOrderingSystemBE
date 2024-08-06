bf656432e369f59b8cb3f6049abcba3494372b6d
Mtri <tranminhtri10213@gmail.com>
2024-08-06 09:52:55 +0700

    remove token from getAllfood

--- src/routes/foodType.js
+++ src/routes/foodType.js
@@ -2,7 +2,7 @@ const foodTypeController = require('../controllers/foodTypeController');
 const middlewareController = require('../controllers/midlewareController');
 const router = require('express').Router();
 
-router.get('/',middlewareController.verifyToken, foodTypeController.getAllFoodTypes);
+router.get('/', foodTypeController.getAllFoodTypes);
 router.post('/', middlewareController.verifyToken, foodTypeController.addFoodType);
 router.get('/:id',middlewareController.verifyToken, foodTypeController.getFoodType);
 router.put('/:id',middlewareController.verifyToken, foodTypeController.updateFoodType);
